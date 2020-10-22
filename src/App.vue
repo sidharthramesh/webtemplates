d07b8206c05a2b3202ea354e793de61e3d98d957
sidharth.ramesh <tornadoalert@gmail.com>
2020-10-22 17:37:06 +0530

    working for most types

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 
 <script>
 import Form from './components/Form.vue'
-import webtemplate from './webtemplate.json'
+import webtemplate from './../physical-doc-constrained.json'
 export default {
   name: 'App',
   components: {
--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,13 +1,19 @@
 <template>
-	<div>
-		<h1>Tree</h1>
-		<p>{{ template.tree }}</p>
-		<h1>Visible Input</h1>
-		<p>{{ visibleInputs[3] }}</p>
-		<FormulateForm v-model="formValues" :schema="formulateSchema">
-		</FormulateForm>
-		<p>{{ formValues }}</p>
-	</div>
+	<section class="section">
+		<div class="columns">
+			<div class="column">
+				<div class="box">
+					<h2 class="subtitle">{{ template.tree.name }}</h2>
+					<FormulateForm v-model="formValues" :schema="formulateSchema">
+						<FormulateInput type="submit" input-class="button is-success is-fullwidth">
+							Submit
+						</FormulateInput>
+					</FormulateForm>
+				</div>
+			</div>
+		</div>
+		<pre>{{ formValues }}</pre>
+	</section>
 </template>
 
 <script>
@@ -46,31 +52,70 @@ export default {
 				TEXT: "text",
 				CODED_TEXT: "select",
 				DECIMAL: "number",
+				INTEGER: "number",
 				DATETIME: "datetime-local",
+				DATE: "date",
+				TIME: "time",
+				BOOLEAN: "checkbox"
 			};
-			let inputs = this.visibleInputs.map((visibleInput) => {
-				let { inputs } = visibleInput;
+			let schema = this.visibleInputs.map((visibleInput) => {
+				let { inputs, name, aqlPath } = visibleInput;
+				// Type transformation
+				inputs = inputs.map((input) => ({
+					...input,
+					type: typeMap[input.type],
+				}));
+				// Specific transformations
 				inputs = inputs.map((input) => {
-					input = { ...input, type: typeMap[input.type] };
-					if (input.type == "select") {
-						return { ...input, options: input.list };
-					} else {
-						return input;
+					let { type, list } = input;
+					if (type === "select") {
+						input = { ...input, options: list };
 					}
+					return input;
 				});
-				return { ...visibleInput, inputs: inputs };
-			});
-			let schema = inputs.map((input) => {
-				if (input.inputs.length > 1) {
+				// Styling transformations
+				inputs = inputs.map((input) => {
+					let elementClass;
+					if (input.type === "select") {
+						elementClass = "select";
+					} else {
+						elementClass = "control";
+					}
 					return {
-						type: "group",
-						label: input.name,
-						name: input.aqlPath,
-						children: input.inputs.map((i) => ({ ...i, name: i.suffix })),
+						...input,
+						"outer-class": "field",
+						"label-class": "label",
+						"input-class": "input",
+						"element-class": elementClass,
 					};
+				});
+				if (inputs.length > 1) {
+					// Some styling changes for groups
+					inputs = inputs.map((input) => ({
+						...input,
+						name: input.suffix,
+						"wrapper-class": "field",
+						"outer-class": "column",
+					}));
+					inputs = inputs.map(input=>{
+						if (input.type != "select") {
+							return {...input, placeholder: input.suffix}
+						} else {
+							return input
+						}
+					})
+					let group = {
+						type: "group",
+						children: inputs,
+						label: name,
+						name: aqlPath,
+						"outer-class": "field",
+						"label-class": "label",
+						"groupRepeatable-class": "columns",
+					};
+					return group;
 				} else {
-					let i = input.inputs[0];
-					return { ...i, label: input.name, name: input.aqlPath };
+					return { ...inputs[0], label: name, name: aqlPath };
 				}
 			});
 			return schema;
